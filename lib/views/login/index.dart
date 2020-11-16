7e5653b95144c1835b27dc9418bc478f7443d06b
SAYAADALAHIbrahim <minhold36@gmail.com>
2020-11-16 09:10:21 +0700

    Master

--- lib/views/login/index.dart
+++ lib/views/login/index.dart
@@ -1,19 +1,4 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login',
-      debugShowCheckedModeBanner: false,
-      home: Login(),
-    );
-  }
-}
+part of '../view.dart';
 
 class Login extends StatelessWidget {
   @override
---
+++ lib/views/view.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+part 'login/index.dart';
+part 'register/index.dart';
